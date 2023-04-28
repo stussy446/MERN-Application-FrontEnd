b8a2a7c84e564b4bfb83516fb811eb91b100a672
Steve Rector <steverector27@gmail.com>
2023-04-28 15:45:21 -0700

    Site showing on localhost and providing a body from the form

--- index.js
+++ index.js
@@ -11,4 +11,45 @@ app.use(express.urlencoded({
 app.use(express.static('public'));
 
 // Define global variables for the top and bottom HTML neeeded..
+let htmlTop = `
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8'>
+<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+<title>Steve Rector</title>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+<script src='main.js'></script>
+</head>
+<body>
+<header>
+<h1>Steve Rector</h1>
+</header>
+<nav>
+<a href="index.html">Home</a>
+<a href="contact.html">Contact</a>
+<a href="gallery.html">Gallery</a>
+</nav>
+<main>
+`
+
+let htmlBottom = `
+</article>
+</section>
+</main>
+<footer>
+<p>&copy; 2023 Steve Rector</p>
+</footer>
+</body>
+</html>
+`
+
+app.listen(PORT, () =>{
+    console.log(`Server listening on port ${PORT}`)
+})
+
+app.post("/contact", (req, res) => {
+    console.log(req);
+})
 
